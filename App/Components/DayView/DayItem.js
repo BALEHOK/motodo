6af7943b39b0c291663c3a3eb71a3c6f214cd4bf
BALEHOK <zhmurik@yandex.ru>
2016-08-26 00:33:37 +0300

    importance and notification icons

--- App/Components/DayView/DayItem.js
+++ App/Components/DayView/DayItem.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // Styles
 import styles from './Styles/DayItemStyle';
@@ -13,8 +14,18 @@ export default class DayItem extends React.Component {
     const item = this.props.item;
     return (
       <View style={styles.row}>
+        <View style={styles.itemIcons}>
+          { item.importance
+            ? <Icon name={'ios-alert-outline'} />
+            : null
+          }
+
+          { item.notifEnabled
+            ? <Icon name={'ios-notifications-outline'} />
+            : null
+          }
+        </View>
         <Text style={styles.itemName}>{item.name}</Text>
-        <Text style={styles.itemName}>{item.title}</Text>
       </View>
     );
   }
--- App/Components/DayView/Styles/DayItemStyle.js
+++ App/Components/DayView/Styles/DayItemStyle.js
@@ -1,18 +1,23 @@
-import { StyleSheet } from 'react-native'
-import { ApplicationStyles, Metrics, Colors } from '../../../Themes/'
+import { StyleSheet } from 'react-native';
+import { ApplicationStyles, Metrics, Colors } from '../../../Themes/';
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
   row: {
+    alignItems:'flex-start',
+    backgroundColor: Colors.fire,
     flex: 1,
     flexDirection: 'row',
-    backgroundColor: Colors.fire,
     marginVertical: Metrics.smallMargin,
-    justifyContent: 'center'
+    padding: 10
+  },
+  itemIcons: {
+    alignSelf: 'center',
+    color: Colors.snow,
+    width: 15
   },
   itemName: {
     alignSelf: 'center',
-    color: Colors.snow,
-    textAlign: 'center'
+    color: Colors.snow
   }
-})
+});
