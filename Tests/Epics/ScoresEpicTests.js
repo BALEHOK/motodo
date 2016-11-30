d0f41d51f6cacb1c495a453d3b7d0509031b2fd9
BALEHOK <zhmurik@yandex.ru>
2016-12-01 01:34:48 +0200

    reward test kind of work

--- Tests/Epics/ScoresEpicTests.js
+++ Tests/Epics/ScoresEpicTests.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import sinon from 'sinon';
 import { ActionsObservable } from 'redux-observable';
 import {Observable} from 'rxjs/Observable';
 import scoresEpic from '../../App/Epics/ScoresEpic';
@@ -12,10 +13,35 @@ test.before(() =>
   }
 );
 
-test.only('score test', t => {
+test('no reword received', t => {
   scoresRepoMock.getScore = () => Observable.of(2);
   const action$ = ActionsObservable.of(itemsActionCreators.markDone({time: 1, importance: 0}));
 
   return scoresEpic(action$)
     .subscribe(() => t.pass());
+});
+
+test('reword 1 received', t => {
+  scoresRepoMock.getScore = () => Observable.of(8);
+  const action$ = ActionsObservable.of(itemsActionCreators.markDone({time: 1, importance: 0}));
+
+  return scoresEpic(action$)
+    .subscribe(() => t.pass());
+});
+
+test('reword 2 received', t => {
+  scoresRepoMock.getScore = () => Observable.of(25);
+  const action$ = ActionsObservable.of(itemsActionCreators.markDone({time: 1, importance: 0}));
+
+  return scoresEpic(action$)
+    .subscribe(() => t.pass());
+});
+
+test('reword 3 received', t => {
+  let spy = sinon.spy(scoresRepoMock, 'saveScore');
+  scoresRepoMock.getScore = () => Observable.of(49);
+  const action$ = ActionsObservable.of(itemsActionCreators.markDone({time: 3, importance: 1}));
+
+  return scoresEpic(action$)
+    .subscribe(() => t.true(spy.calledWith(2)));
 });
